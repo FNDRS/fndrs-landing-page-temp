298525700d9b32a6a302688964d12ad4714701b0
mgeovany <marlongeo1999@gmail.com>
2024-11-15 17:34:03 -0600

    Initialize project with Astro, TypeScript, and Tailwind CSS setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      letterSpacing: {
+        separated: "0.35px",
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
+  },
+  plugins: [
+    ({ addUtilities }) => {
+      const newUtilities = {
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      };
+
+      addUtilities(newUtilities);
+    },
+  ],
+};
