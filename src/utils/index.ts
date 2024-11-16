5e4e7d877c59e79698c00574a734842c58ee69d3
mgeovany <marlongeo1999@gmail.com>
2024-11-15 20:16:48 -0600

    styles: Add initial layout, header, and global styles; include SVG icons and utility constants

---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export const BASE_DOMAIN = "FNDRS.com";
+export const BASE_URL = "FNDRS.com";
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Jost", ...defaultTheme.fontFamily.sans],
+    },
     extend: {
+      colors: {
+        "primary-black": "#131313",
+      },
       letterSpacing: {
         separated: "0.35px",
       },
